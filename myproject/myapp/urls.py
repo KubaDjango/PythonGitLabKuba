6e84f65f6d01aff58eec519fc5e897b2e212384d
Kuba <jakubowsiak@gmail.com>
2025-05-24 23:51:38 +0200

    go_6

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import hello_world, hello_name, add , div, table, update_post, post_list, create_post
+
+#create_post, delete_post, post_list
+
+urlpatterns = [
+    path('hello/', hello_world),
+    path('hello/<str:name>/', hello_name),
+    path('add/<int:a>/<int:b>/', add),
+    path('div/<int:a>/<int:b>/', div),
+    path('table/', table),
+
+    path('posts/',post_list, name="post_list"),
+    path('posts/create/', create_post, name='create_post'),
+    path('post/update/<int:post_id>', update_post, name='delete_post'),
+
+]
---
+++ myproject/myapp/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from gc import get_objects
+
+from django.template.defaultfilters import title
+
+from .forms import LinearEquationForm, PostForm, ImageForm
+
+from django.http import HttpResponse, JsonResponse
+
+from .models import Post, Image
+
+
+# Create your views here.
+
+
+def hello_world(request):
+    #return HttpResponse('Hello World')
+    return render(request,'hello_world.html')
+
+def hello_name(request,name):
+    #return HttpResponse(f'Hello {name}')
+    return render(request, 'hello_name.html',{'name':name, 'klucz':'wartość'})
+
+def add(request,a,b):
+    result = a + b
+    return render(request,'add.html',{'a':a,'b':b,'c':result})
+
+def div(request,a,b):
+    if b!=0:
+        c = a / b
+    else:
+        c = None
+    return render(request,'div.html',{'a':a, 'b':b, 'c':c })
+
+def table(request):
+    posts = [
+        {'id':1,'title':'Pierwszy Post','content':'Tresc pierwszego posta','create at':"2025-05-19"},
+        {'id':2,'title': '2 Post', 'content': 'Tresc 2 posta', 'create at': "2025-05-19"},
+        {'id':3,'title': '3 Post', 'content': 'Tresc 3 posta', 'create at': "2025-05-19"},
+    ]
+    for post in posts:
+        print(f"Lp.{post['id']}, Tytuł: {post['title']}, Tresc:{post['content']}, Data {post['create at']}")
+    return render(request,'table.html')
+
+def create_post(request):
+    if request.method == "POST":
+        # title = request.POST.get("title")
+        # content = request.POST.get("content")
+        # if title and content:
+        #     Post.objects.create(title=title,content=content)
+        # else:
+        #     return HttpResponse("Nie wszystkie dane są podane")
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'myapp/post_form.html',{'form':form,'title':'Dodaj post'})
+
+#
+def delete_post(request,post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST' and request.POST.get('_method') == 'DELETE':
+        post.delete()
+        return redirect("post_list")
+    return render(request, 'myapp/delete_post.html', {"post":post})
+
+
+def post_list(request):
+    sort = request.GET.get('sort', 'id')
+    posts = Post.objects.all().order_by(sort)
+    return render(request, 'myapp/post_list.html', {'posts': posts})
+
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+
+    if request.method == "POST" and form.is_valid():
+        #post.title = request.POST.get("title")
+        #post.content = request.POST.get("content")
+        #post.save()
+        form.save()
+        return redirect("post_list")
+
+    return render(request, 'myapp/post_form.html', {'form': form, 'title': 'Edytuj post'})
+
+
+# def zero_point(request):
+#     if request.method == 'POST':
+#         form = LinearEquationForm(request.POST)
+#         if form.is_valid():
+#             a = form.cleaned_data['a']
+#             b = form.cleaned_data['b']
+#     form = LinearEquationForm()
+#     return render(request, 'myapp/zero_point.html', {'form':form})
