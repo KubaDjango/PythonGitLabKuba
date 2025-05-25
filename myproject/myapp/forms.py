abbd3be194e31a9ed929acd0dc960f45a69ff7c2
Kuba <jakubowsiak@gmail.com>
2025-05-25 19:44:40 +0200

    go_19.44

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -18,32 +18,3 @@ class MyForm(forms.Form):
 
 
 
-
-#
-#
-#
-# ################
-# class LinearEquationForm(forms.Form):
-#     a = forms.FloatField(label="Współczynnik a")
-#     b = forms.FloatField(label="Współczynnik b")
-#
-#
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ["title", "content"]
-#         labels = {"title": "Tytuł posta", "content": "Treść posta"}
-#
-#
-# class ImageForm(forms.ModelForm):
-#     class Meta:
-#         model = Image
-#         fields = ["title", "image"]
-#         labels = {"title": "Tytuł obrazka", "image": "Obrazek"}
-#
-#     def clean_image(self):
-#         image = self.cleaned_data.get('image')
-#         if image:
-#             if not image.name.lower().endswith('.jpg'):
-#                 raise forms.ValidationError("Tylko pliki JGP są dozwolone!")
-#         return image
