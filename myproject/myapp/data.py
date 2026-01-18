d83dfaac693374661d477f7058c2d544a9df40da
Kuba <jakubowsiak@gmail.com>
2026-01-18 13:31:51 +0100

    not using data in forms

--- myproject/myapp/data.py
+++ myproject/myapp/data.py
@@ -1,5 +1,5 @@
-TOKEN = "glpat-rfQjNVf_r9Ie5o1tqJgHIm86MQp1OmgzYXkyCw.01.121usgre3"
+#TOKEN = "glpat-rfQjNVf_r9Ie5o1tqJgHIm86MQp1OmgzYXkyCw.01.121usgre3"
 
-gitlab_location="kuba9223326"
+#gitlab_location="kuba9223326"
 
 #gitlab_toplevel= "https://gitlab.com/groups/" & gitlab_location & "/-/epics?state=opened&page=1&sort=start_date_desc&label_name%5B%5D=Initiative%20Level%3A%3ATop%20Level"
--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Post
-from data import gitlab_location
+#from data import gitlab_location
 
 class MyForm(forms.Form):
     name = forms.CharField(label='NAME OF YOUR PROJECT',max_length= 100)
